dddffd60180460e4252d5dd1d32e46abc99bb240
abhyudy <abhyudyasinghujala2002@gmail.com>
2022-09-15 22:02:41 +0530

    completed entertainment card and completed poster section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 
-// REact Slick Css
+// Routing 
+import { Routes, Route } from "react-router-dom";
+
+// React Slick Css
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// Pages
+import MoviePage from "./pages/Movie.page";
+import HomePage from "./pages/Home.page";
+import PlayPage from "./pages/play.page";
+
 function App() {
   return (
-    <div>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<PlayPage />} />
+    </Routes>
   );
 }
 
---
+++ src/components/Poster/Poster.Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Poster = (props) => {
+  return (
+    <div>
+      {props.original_title}
+    </div>
+  )
+}
+
+export default Poster;
